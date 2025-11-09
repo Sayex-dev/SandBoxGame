688d091abadb5b913a1137911315673faa5ff891
DESKTOP-VK47P0A\Simon <simi.hori@hotmail.com>
2025-11-09 17:47:52 +0100

    Fixed mesh creation.

--- src/world/chunk.gd
+++ src/world/chunk.gd
@@ -1,68 +1,64 @@
-extends Node
+extends MeshInstance3D
 
-class_name Chunk
-
-var normals = [
-	Vector3i(1, 0, 0), 
-	Vector3i(-1, 0, 0), 
-	Vector3i(0, 1, 0), 
-	Vector3i(0, -1, 0),
-	Vector3i(0, 0, 1), 
-	Vector3i(0, 0, -1)
-]
+class_name Chunk 
 
 class Surface:
 	var vertices: Array[Vector3] = []
-	var uvs: Array[Vector2] = []
+	var indices: Array[int] = []
+	var normal: Vector3
 	
-	func _init(_vertices: PackedVector3Array, _uvs: PackedVector2Array):
+	func _init(_vertices: Array[Vector3], _indices: Array[int], _normal: Vector3):
 		vertices = _vertices
-		uvs = _uvs
+		normal = _normal
 
-var _mesh: MeshInstance3D
-var _chunk_size: Vector3i
-var _blocks: Array[int]
+var normals = [
+	Vector3i(1, 0, 0),	# x+
+	Vector3i(-1, 0, 0),	# x-
+	Vector3i(0, 1, 0),	# y+
+	Vector3i(0, -1, 0),	# y-
+	Vector3i(0, 0, 1),	# z+
+	Vector3i(0, 0, -1)	# z-
+]
+var chunk_size: Vector3i
+var blocks: Array[int]
 
-func _init(chunk_size: Vector3i) -> void:
-	_chunk_size = chunk_size
-	_blocks = []
+func _init(_chunk_size: Vector3i) -> void:
+	chunk_size = _chunk_size
+	blocks = []
 	var block_count = _chunk_size.x * _chunk_size.y * _chunk_size.z
-	_blocks.resize(block_count)
-	_blocks.fill(-1)
+	blocks.resize(block_count)
+	blocks.fill(-1)
 
 func get_block(chunk_pos: Vector3i) -> int:
 	var index: int = chunk_to_array_pos(chunk_pos)
-	assert (not is_in_chunk(index), "Pos not in chunk: " + str(chunk_pos))
-		
-	return _blocks[index]
+	return blocks[index]
 
 func set_block(chunk_pos: Vector3i, block_id: int):
 	var index: int = chunk_to_array_pos(chunk_pos)
-	assert (not is_in_chunk(index), "Pos not in chunk: " + str(chunk_pos))
-	_blocks[index] = block_id
+	blocks[index] = block_id
 	
 func chunk_to_array_pos(chunk_pos: Vector3i) -> int:
-	var index: int = chunk_pos.x + chunk_pos.y * _chunk_size.x + chunk_pos.z * _chunk_size.x * _chunk_size.y
+	var index: int = chunk_pos.x + chunk_pos.y * chunk_size.x + chunk_pos.z * chunk_size.x * chunk_size.y
 	return index
 
 func array_to_chunk_pos(index: int) -> Vector3i:
-	var x = index % _chunk_size.x
+	var x = index % chunk_size.x
 	@warning_ignore("integer_division")
-	var y = int(index / _chunk_size.x) % _chunk_size.y
+	var y = int(index / chunk_size.x) % chunk_size.y
 	@warning_ignore("integer_division")
-	var z = int(index / _chunk_size.x) * _chunk_size.y
+	var z = int(index / chunk_size.x) * chunk_size.y
 	return Vector3i(x, y, z)
 
 func is_in_chunk(index: int) -> bool:
-	return 0 >= index and index < (_chunk_size.x * _chunk_size.y * _chunk_size.z)
+	return 0 >= index and index < (chunk_size.x * chunk_size.y * chunk_size.z)
 
 func find_block_surfaces(
-	x_pos_chunk: Chunk,
-	x_neg_chunk: Chunk,
-	y_pos_chunk: Chunk,
-	y_neg_chunk: Chunk,
-	z_pos_chunk: Chunk,
-	z_neg_chunk: Chunk
+	x_pos_chunk: Chunk = null,
+	x_neg_chunk: Chunk = null,
+	y_pos_chunk: Chunk = null,
+	y_neg_chunk: Chunk = null,
+	z_pos_chunk: Chunk = null,
+	z_neg_chunk: Chunk = null
 ) -> Dictionary:
 	
 	# Find all exposed block surfaces
@@ -75,9 +71,9 @@ func find_block_surfaces(
 		z_pos_chunk,
 		z_neg_chunk
 	]
-	for z in range(_chunk_size.z):
-		for y in range(_chunk_size.y):
-			for x in range(_chunk_size.x):
+	for z in range(chunk_size.z):
+		for y in range(chunk_size.y):
+			for x in range(chunk_size.x):
 				var block_pos = Vector3i(x, y, z)
 				
 				# Skip Air Blocks
@@ -95,8 +91,11 @@ func find_block_surfaces(
 					if is_in_chunk(chunk_to_array_pos(adjacent_pos)):
 						adjacent_block = get_block(adjacent_pos)
 					else:
-						var adjacent_chunk_pos = adjacent_pos % _chunk_size
-						adjacent_block = adjacent_chunks[i].get_block(adjacent_chunk_pos)
+						var adjacent_chunk_pos = adjacent_pos % chunk_size
+						if adjacent_chunks[i]:
+							adjacent_block = adjacent_chunks[i].get_block(adjacent_chunk_pos)
+						else:
+							adjacent_block = -1
 					if adjacent_block != -1:
 						has_exposed_surfaces = true
 						exposed_surfaces[i] = true
@@ -109,8 +108,8 @@ func get_surface_vectors(exposed_block_surfaces: Dictionary) -> Array[Surface]:
 	for pos in exposed_block_surfaces:
 		block_surfaces[pos] = exposed_block_surfaces[pos].duplicate()
 	
-	var max_dim: int = max(_chunk_size.x, _chunk_size.y, _chunk_size.z)
-	var surface_vector_dict: Array[Surface] = []
+	var max_dim: int = max(chunk_size.x, chunk_size.y, chunk_size.z)
+	var surfaces: Array[Surface] = []
 	while len(block_surfaces) > 0:
 		# Find a surface on a block
 		# Example:
@@ -201,37 +200,25 @@ func get_surface_vectors(exposed_block_surfaces: Dictionary) -> Array[Surface]:
 					var remove_pos = Vector3i(x, y, z)
 					block_surfaces[remove_pos][dir_i] = false
 		
-		# Find corner vectors of surface
-		# This vector converts the block coordinate min pos to world coordinate of the surface
-		var min_conversion_vectors = [
-			Vector3i(1, 0, 0),
-			Vector3i(0, 0, 0),
-			Vector3i(0, 1, 0),
-			Vector3i(0, 0, 0),
-			Vector3i(0, 0, 1),
-			Vector3i(0, 0, 0),
-		] 
-		# This vector converts the block coordinate max pos to world coordinate of the surface
-		var max_conversion_vectors = [
-			Vector3i(1, 1, 1),
-			Vector3i(0, 1, 1),
-			Vector3i(1, 1, 1),
-			Vector3i(1, 0, 1),
-			Vector3i(1, 1, 1),
-			Vector3i(1, 1, 0),
-		] 
-		var min_world_pos = min_pos + min_conversion_vectors[dir_i]
-		var max_world_pos = max_pos + min_conversion_vectors[dir_i]
-		
-		# Find the corner vertices
-		# TODO: Find the corner vertices
-		
-		# Compute vertices
+		# Find corner vectors of surface in world space
 		var vertices: PackedVector3Array = PackedVector3Array()
-		var uvs: PackedVector2Array = PackedVector2Array()
+		var indices: PackedInt32Array = PackedInt32Array()
 		
-		var surface: Surface = Surface.new(vertices, uvs)
-	return surface_vector_dict
+		var surface_displace: Vector3i = Vector3i(dir_i == 0, dir_i == 2, dir_i == 0 or dir_i == 2 or dir_i == 4)
+		var corner_1: Vector3i = Vector3i(embed_2d_in_plane(Vector2i(0, 0), normal)) + min_pos + surface_displace
+		var corner_2: Vector3i = Vector3i(embed_2d_in_plane(Vector2i(0, 1), normal)) + surface_displace
+		var corner_3: Vector3i = Vector3i(embed_2d_in_plane(Vector2i(1, 1), normal)) + max_pos + surface_displace
+		var corner_4: Vector3i = Vector3i(embed_2d_in_plane(Vector2i(1, 0), normal)) + surface_displace
+		
+		vertices.append(corner_1)
+		vertices.append(corner_2)
+		vertices.append(corner_3)
+		vertices.append(corner_4)
+		indices.append_array([0, 1, 2, 1, 3, 2])
+		
+		var surface: Surface = Surface.new(vertices, indices, normal)
+		surfaces.append(surface)
+	return surfaces
 
 func embed_2d_in_plane(v: Vector2, n: Vector3) -> Vector3:
 	var a: Vector3
@@ -245,12 +232,12 @@ func embed_2d_in_plane(v: Vector2, n: Vector3) -> Vector3:
 	return a.x * t1 + a.y * t2
 
 func build_chunk_mesh(
-	x_pos_chunk: Chunk,
-	x_neg_chunk: Chunk,
-	y_pos_chunk: Chunk,
-	y_neg_chunk: Chunk,
-	z_pos_chunk: Chunk,
-	z_neg_chunk: Chunk
+	x_pos_chunk: Chunk = null,
+	x_neg_chunk: Chunk = null,
+	y_pos_chunk: Chunk = null,
+	y_neg_chunk: Chunk = null,
+	z_pos_chunk: Chunk = null,
+	z_neg_chunk: Chunk = null
 ):
 	var exposed_block_surfaces = find_block_surfaces(
 		x_pos_chunk,
@@ -260,32 +247,21 @@ func build_chunk_mesh(
 		z_pos_chunk,
 		z_neg_chunk
 	)
-	var surfaces = get_surface_vectors(exposed_block_surfaces)
-	
-	var tmpMesh = Mesh.new()
-	var vertices = PackedVector3Array()
-	
-	vertices.push_back(Vector3(1,0,0))
-	vertices.push_back(Vector3(1,0,1))
-	vertices.push_back(Vector3(0,0,1))
-	vertices.push_back(Vector3(0,0,0))
-	
-	UVs.push_back(Vector2(0,0))
-	UVs.push_back(Vector2(0,1))
-	UVs.push_back(Vector2(1,1))
-	UVs.push_back(Vector2(1,0))
-	
-	mat.albedo_color = color
+	var surfaces: Array[Surface] = get_surface_vectors(exposed_block_surfaces)
 	
 	var st = SurfaceTool.new()
-	st.begin(Mesh.PRIMITIVE_TRIANGLE_STRIP)
-	st.set_material(mat)
+	st.begin(Mesh.PRIMITIVE_TRIANGLES)
+	st.set_color(Color.AQUA)
 	
-	for v in vertices.size(): 
-		st.add_color(color)
-		st.add_uv(UVs[v])
-		st.add_vertex(vertices[v])
-	
-	st.commit(tmpMesh)
-	
-	$MeshInstance.mesh = tmpMesh
+	for i in len(surfaces):
+		var surface: Surface = surfaces[i]
+		st.set_normal(surface.normal)
+		# Add vertices
+		for vertex in surface.vertices:
+			st.add_vertex(vertex)
+		# Add indexes
+		for index in surface.indices:
+			st.add_index(index + (i * 4))
+		
+	var tmp_mesh: Mesh = st.commit()
+	mesh = tmp_mesh
--- src/world/world_controller.gd
+++ src/world/world_controller.gd
@@ -1,13 +1,13 @@
-extends Node
+extends Node3D
 class_name WorldController
-
 
 class BlockWorld:
 	var loaded_chunks: Dictionary
-
+	
 	func add_chunk(chunk_index: Vector3i, chunk: Chunk):
 		assert (chunk_index not in loaded_chunks, "Chunk already exists and cannot be overwritten.")
 		loaded_chunks[chunk_index] = chunk
+		chunk.build_chunk_mesh()
 
 	func remove_chunk(chunk_index: Vector3i):
 		loaded_chunks.erase(chunk_index)
@@ -15,47 +15,9 @@ class BlockWorld:
 var block_world: BlockWorld
 var world_mesh: MeshInstance3D
 
-
-
-var UVs = PackedVector2Array()
-var mat = StandardMaterial3D.new()
-var color = Color(0.9, 0.1, 0.1)
-
-func _find_start_pos_in_chunk(chunk: Chunk):
-
-
-func _build_chunk_mesh(chunk: Chunk):
-	# Find start position
-	var start_pos
-	for i in range(len(chunk._blocks)):
-		if chunk._blocks[i] > -1:
-			start_pos = chunk.array_to_chunk_pos(i)
-			break
-	
-	var tmpMesh = Mesh.new()
-	var vertices = PackedVector3Array()
-	
-	vertices.push_back(Vector3(1,0,0))
-	vertices.push_back(Vector3(1,0,1))
-	vertices.push_back(Vector3(0,0,1))
-	vertices.push_back(Vector3(0,0,0))
-	
-	UVs.push_back(Vector2(0,0))
-	UVs.push_back(Vector2(0,1))
-	UVs.push_back(Vector2(1,1))
-	UVs.push_back(Vector2(1,0))
-	
-	mat.albedo_color = color
-	
-	var st = SurfaceTool.new()
-	st.begin(Mesh.PRIMITIVE_TRIANGLE_STRIP)
-	st.set_material(mat)
-	
-	for v in vertices.size(): 
-		st.add_color(color)
-		st.add_uv(UVs[v])
-		st.add_vertex(vertices[v])
-	
-	st.commit(tmpMesh)
-	
-	$MeshInstance.mesh = tmpMesh
+func _ready():
+	var chunk = Chunk.new(Vector3i(4, 4, 4))
+	chunk.set_block(Vector3i(0, 0, 0), 0)
+	block_world = BlockWorld.new()
+	block_world.add_chunk(Vector3i(0, 0, 0), chunk)
+	add_child(chunk)
