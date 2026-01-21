1df8eebb5409b68c3c080d6b8c84061cf96aacce
simonhorat <simon.horat@cudos.ch>
2026-01-21 16:41:01 +0100

    stash.

---
+++ src/world/construct/ConstrcutVisuals.cs
@@ -0,0 +1,44 @@
+using System.Collections.Generic;
+using Godot;
+
+public partial class ConstructVisuals : Node3D
+{
+    private float moduleSize;
+    private Dictionary<ModuleLocation, MeshInstance3D> activeModules = new();
+    private LinkedList<MeshInstance3D> modulePool = new();
+
+    public void Initialize(float moduleSize, int initialPoolSize = 0)
+    {
+        this.moduleSize = moduleSize;
+        for (int i = 0; i < initialPoolSize; i++)
+        {
+            ExtendModulePool();
+        }
+    }
+
+    public void AddModule(ModuleLocation loc, Mesh moduleMesh)
+    {
+        RemoveModule(loc);
+        if (modulePool.First == null) ExtendModulePool();
+        var module = modulePool.First.Value;
+        modulePool.RemoveFirst();
+        module.Mesh = moduleMesh;
+        activeModules[loc] = module;
+        module.Position = (Vector3)loc.Value * moduleSize;
+    }
+
+    public void RemoveModule(ModuleLocation loc)
+    {
+        activeModules.Remove(loc, out MeshInstance3D module);
+        if (module == null) return;
+        module.Mesh = null;
+        modulePool.AddFirst(module);
+    }
+
+    private void ExtendModulePool()
+    {
+        MeshInstance3D newModule = new MeshInstance3D();
+        modulePool.AddFirst(newModule);
+        AddChild(newModule);
+    }
+}
